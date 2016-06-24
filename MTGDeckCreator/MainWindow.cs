665d59e0f47e4070d6ad1d1be86101c2839092ef
ecia <ewa.szklanny95@gmail.com>
2016-06-24 12:22:38 +0200

    Previously not commited

--- MTGDeckCreator/MainWindow.cs
+++ MTGDeckCreator/MainWindow.cs
@@ -16,50 +16,86 @@ namespace MTGDeckCreator
         public MainWindow()
         {
             InitializeComponent();
+            sql = new SQLDataExchange("localhost", "root", "", "mtg");
+            deck = new Deck();
+            cards = sql.GetCardsList();
             deckTable = InterfaceOperations.createTable(columnNames);
             cardsTable = InterfaceOperations.createTable(columnNames);
             cardLibraryView.DataSource = cardsTable;
             deckView.DataSource = deckTable;
             cardViewPanel = CardViewPanel.CreateGraphics();
-            
-            for (int i = 1; i<3; i++)
-            {
-                cardsTable.Rows.Add(i.ToString(), "Zestaw"+i, "Typ"+i);
-            }
+
+            foreach (SpellCard card in cards) addToDataTable(cardsTable, card);
+
+            //deck.addCard(cards[1]);
         }
 
+        private SQLDataExchange sql;
+        private Deck deck;
+        private List<SpellCard> cards;
         private Graphics cardViewPanel;
         private Options optionsPanel;
         private DataTable deckTable = new DataTable();
         private DataTable cardsTable = new DataTable();
         private string currentCardImageLocation = "";
-        string[] columnNames = { "Name", "Set", "SuperTypes", "Types", "SubTypes", "Rarity" }; //,"RulesText","Flavor","Artist","Numer","MultiverseID","ManaCost"};
-        
+        string[] columnNames = { "Name", "Set", "SuperTypes", "Types", "SubTypes", "Rarity" ,"RulesText","Flavor","Artist","Numer","MultiverseID","Picture","ManaCost"};
+
+        private void addToDataTable(DataTable data, SpellCard c)
+        {
+            data.Rows.Add(new object[] { c.Name, c.Set, c.SuperTypes[0], c.Types[0], c.SubTypes[0], c.Rarity, c.RulesText, c.Flavor, c.Artist, c.Number, c.MultiverseID, c.Picture, c.ManaCost});
+        }
 
         private void modifyColumns(string[] s)
         {
             columnNames = s;
             loadTables();
+            //deckView.Columns[0].data
+        }
+
+        private void columnsFillMode(bool b)
+        {
+            if (b)
+            {
+                cardLibraryView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                deckView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            }
+            else
+            {
+                cardLibraryView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                deckView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            }
+        }
+
+        private void viewStats()
+        {
+            manaLabel.Text = deck.calculateManaCurve().ToString();
+            cardsLabel.Text = deck.CardsCount.ToString();
+            landCardsLabel.Text = deck.calculateLandCount().ToString();
+            creatureCardsLabel.Text = deck.calculateCreatureCount().ToString();
+            otherCardsLabel.Text = deck.calculateOtherCount().ToString();
         }
 
         private void loadTables()
         {
             //wyświetlanie tylko tych kolumn z datasource, których nazwy znajdują się w columnNames
+           // cardLibraryView.DataSource = cardsTable.Columns.
         }
+
 
 
         private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
         {
             optionsPanel = new Options(this);
-            optionsPanel.update += modifyColumns;
+            optionsPanel.columnsUpdate += modifyColumns;
+            optionsPanel.columnsModeChange += columnsFillMode;
             optionsPanel.Show();
         }
-        
+
         private void infoToolStripMenuItem_Click(object sender, EventArgs e)
         {
             //SQLDataExchange sql = new SQLDataExchange("localhost", "root", "toor", "mtg");
             //SpellCard list = sql.GetCardInfo("Beton");
-            MessageBox.Show("MTGDeckCreator \n Autorzy: \tJakub Rup \n \tEwa Szklanny \n","Informacje");
+            MessageBox.Show("MTGDeckCreator \n Authors: \tJakub Rup \n \tEwa Szklanny \n", "Informations");
         }
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
@@ -83,32 +119,42 @@ namespace MTGDeckCreator
             }
         }
 
+        private SpellCard findCardByID( int id )
+        {
+            foreach (SpellCard card in cards)
+                if ((int)card.MultiverseID == id) return card;
+
+            return null;
+        }
+
+        private string getValueFromCell(DataGridViewRow row, string heder = "Name")
+        {
+            foreach (DataGridViewCell cell in row.Cells)
+                if (cell.OwningColumn.HeaderText == heder) return cell.Value.ToString();
+            return "";
+        }
+        
         private void addButton_Click(object sender, EventArgs e)
         {
             if (cardLibraryView.SelectedRows.Count > 0)
-            foreach(DataGridViewRow row in cardLibraryView.SelectedRows)
-             InterfaceOperations.addRow(((DataRowView)cardLibraryView.SelectedRows[row.Index].DataBoundItem).Row, ref deckTable);
+            {
+                foreach (DataGridViewRow row in cardLibraryView.SelectedRows)
+                {
+                    SpellCard c = sql.GetCardInfo(getValueFromCell(row));
+
+                    addToDataTable(deckTable, c);
+                    deck.addCard(c);
+                }
+                //viewStats();
+            }
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
-            if(currentCardImageLocation != "")
-                InterfaceOperations.drawImage(cardViewPanel, currentCardImageLocation, CardViewPanel.Size.Width, CardViewPanel.Size.Height);
+           // if (currentCardImageLocation != "")
+              //  InterfaceOperations.drawImage(cardViewPanel, currentCardImageLocation, CardViewPanel.Size.Width, CardViewPanel.Size.Height);
         }
-
-        //private IEnumerator<string> returnCell( DataGridViewRow r)
-        //{
-        //    for (int i = 0; i < r.Cells.Count; i++)
-        //        yield return r.Cells[i].Value.ToString();
-        //    yield break;
-        //}
-
-        //private bool checkIfContains(string number)
-        //{
-        //    deckTable.Rows.Contains(number);
-        //    return false;
-        //}
 
         private void deleteButton_Click(object sender, EventArgs e)
         {
@@ -124,19 +170,16 @@ namespace MTGDeckCreator
 
         private void delete4Button_Click(object sender, EventArgs e)
         {
-           
-        }
-
-        private void ssSet(object sender, EventArgs e)
-        {
 
         }
 
         private void deckView_RowEnter(object sender, DataGridViewCellEventArgs e)
         {
-                string imagename = deckView.Rows[e.RowIndex].Cells[0].Value.ToString() + ".jpg";
-                currentCardImageLocation = @"C:\Users\Admin\Desktop\Projekt\MTGDeckCreator\MTGDeckCreator\" + imagename;
-                InterfaceOperations.drawImage(cardViewPanel, currentCardImageLocation,CardViewPanel.Size.Width, CardViewPanel.Size.Height);
+            string imagename = deckView.Rows[e.RowIndex].Cells["Picture"].Value.ToString();
+            currentCardImageLocation = @"C:\Users\Admin\Desktop\Projekt\MTGDeckCreator\MTGDeckCreator\" + imagename;
+
+            if (File.Exists(currentCardImageLocation)) 
+                InterfaceOperations.drawImage(cardViewPanel, currentCardImageLocation, CardViewPanel.Size.Width, CardViewPanel.Size.Height);
         }
 
         private void cardLibraryView_KeyPress(object sender, KeyPressEventArgs e)
@@ -144,5 +187,6 @@ namespace MTGDeckCreator
             if (e.KeyChar == '\n' && cardLibraryView.SelectedRows.Count > 0)
                 addButton_Click(sender, e);
         }
+        
     }
 }
